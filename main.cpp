ce79600aaf9a146901080375a0d5b6ce9c5e8cd2
mgotao <m.gotao@yahoo.com>
2017-03-30 22:29:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	if(argc < 2)
-	{
-		cout << "You did not give a file name." << endl;
-		return 0;
+	if (argc < 2 || argc >= 3){	//instructions for when no filename passed in command line or incorrect argument taken
+		cout << "This program runs a simulation of the registrar.\n";
+		cout << "The program must be run with an accompanying file.\n";
+		cout << "EX: './a.out /home/cpsc/Documents/foo.txt'\n";
 	}
 
 	string fileName = argv[1];
@@ -46,4 +46,14 @@ int main(int argc, char ** argv)
 	{
 		cout << fileData[i] << endl;
 	}
+	
+	int numWindows = 0;
+		inFile >> numWindows;
+		GenQ line = GenQ(50);
+		Window registrar[numWindows];
+		int simTime = 0;
+		int lineEmpty = 0, windowsOpen = 0;
+		while(lineEmpty < 0 && windowsOpen < 0){
+			
+		}
 }
